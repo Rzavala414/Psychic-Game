6024d6f29ccab4cf59b539df99b7dd42c5dc6434
Roger Zavala <Rogeliozavala15@gmail.com>
2019-09-05 12:33:48 -0700

    updated users guess-text id to work correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,32 @@
 //array to store with all letters
-let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 
 //scores and guesses counter variables
 let wins = 0;
@@ -10,39 +37,37 @@ let guessedLetters = [];
 let winsText = document.getElementById("wins_text");
 let lossesText = document.getElementById("losses_text");
 let guessesLeftText = document.getElementById("guesses_left_text");
-let guessMadeText = document.getElementById("guess_made_text");
+let guessMadeText = document.getElementById("guesses_made_text");
 
 //makes the computer choose a random letter
 let computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
-console.log(computerGuess)
+console.log(computerGuess);
 
 // collects user input for what character is pressed
-document.onkeyup = function (event) {
-    let userGuess = event.key;
-    console.log(userGuess)
+document.onkeyup = function(event) {
+  let userGuess = event.key;
+  console.log(userGuess);
 
-    //check if user guess correctly add a point to wins counter. restart game when either of the above statements is true
-    if (userGuess === computerGuess) {
-        wins++;
-        guessedLetters = [];
-        guessesLeft = 9;
-    }
-    //check if the computer and user chose the same character. if user guess wrong all nine times add a point for losses counter
-    else {
-        guessesLeft--;
+  //check if user guess correctly add a point to wins counter. restart game when either of the above statements is true
+  if (userGuess === computerGuess) {
+    wins++;
+    guessedLetters = [];
+    guessesLeft = 9;
+  }
+  //check if the computer and user chose the same character. if user guess wrong all nine times add a point for losses counter
+  else {
+    guessesLeft--;
+  }
+  //checks if guesses is 0 and takes away a guess
+  if (guessesLeft === 0) {
+    losses++;
+    guessedLetters = [];
+    guessesLeft = 9;
+  }
 
-    }
-    //checks if guesses is 0 and takes away a guess 
-    if (guessesLeft === 0) {
-        losses++;
-        guessedLetters = [];
-        guessesLeft = 9;
-
-    }
-
-    guessMadeText.textContent = guessedLetters;
-    guessesLeftText.textContent = guessesLeft;
-    winsText.textContent = wins;
-    lossesText.textContent = losses;
-}
+  guessMadeText.textContent = guessedLetters;
+  guessesLeftText.textContent = guessesLeft;
+  winsText.textContent = wins;
+  lossesText.textContent = losses;
+};
